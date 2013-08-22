90808a69649db96e9cbdd51dab0942ef58718ccf
jeanluca <lucas@calje.eu>
2013-08-22 15:45:23 +0200

    cleanup + added test for 'preserve' argument

--- specs/ClassicalInheritanceSpec.js
+++ specs/ClassicalInheritanceSpec.js
@@ -1,36 +1,39 @@
 describe('Javascript Classical Inheritance', function () {
 
     // setup classes/prototypes
-    var Base = Object.$augment((function () { // create closure around constructor
-            var value = 0;
+    var baseProto = (function() { // closure, Module Pattern
+        var value = 0;
 
-            function Base(input) { // create closure
-                value = input;
-                this.name = 'Base';
-            }
+        function Base(input) { // create closure
+            value = input;
+            this.name = 'Base';
+        }
 
-            Base.prototype.getValue = function () {
-                return this.multi * value;
-            } ;
-            return Base;
-        })(), {
+        Base.prototype.getValue = function () {
+            return this.multi * value;
+        } ;
+        return Base ;
+    })() ;
+    var Base = Object.$augment( baseProto, {        // the third param is added to the prototype
                 toString: function () {
                     return this.name ;
                 }, multi: 1
             }
-        ) ;
-        var Bar = Base.$augment(function Bar(input) {
+        )
+        , Bar = Base.$augment(function Bar(input) {
             this.$super(input);
             this.name = 'Bar';
-        }, { multi: 2 }) ;
-        var Foo = Bar.$augment(function (input) {
+        }, { multi: 2 })                            // add multi param to the prototype
+
+        , FooConstructor = function (input) {
             this.$super(input);
             this.name = 'Foo';
-        }, {
+        }
+        , Foo = Bar.$augment( FooConstructor, {     // extend Bar without changing FooConstructor
             getValue: function () {
                 return this.multi * this.$super();
             }, multi: 3
-        }, true)
+        }, true)                                    // should be TRUE, otherwise the $augment will modify FooConstructor
         , foo, bar;
 
     beforeEach(function () {
@@ -38,10 +41,13 @@ describe('Javascript Classical Inheritance', function () {
         foo = Foo.$new(100);
     });
 
-    it('should create new instances using Foo', function () {
+    it('should create new instances', function () {
         expect(Base).toBeDefined();
         expect(Bar).toBeDefined();
+        expect(FooConstructor).toBeDefined();
         expect(Foo).toBeDefined();
+        expect(Foo.prototype).not.toBe(FooConstructor.prototype) ; // constructor should be cloned, not referenced
+        expect(Foo).not.toEqual(FooConstructor) ;
         expect(foo).toBeDefined();
         expect(bar).toBeDefined();
 
@@ -64,5 +70,5 @@ describe('Javascript Classical Inheritance', function () {
     it('should throw an exception if overriden method does not exist', function(){
         Foo.prototype.newFunc = function(){ this.$super(); } ;
         expect(foo.newFunc.bind(foo)).toThrow('No overridden method for "newFunc"') ;
-    })
+    });
 });
